bf7966ea502078e25a975422f675dd054bbd13bd
benm4ckenzie <b_m@me.com>
2020-04-09 14:53:34 +0000

    added templates folder and library file

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'book_review'
+app.config["MONGO_URI"] = 'mongodb+srv://benm4ckenzie:c0de1nstitute@myfirstcluster-beoum.mongodb.net/book_review?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Hello world!'
+@app.route('/get_library')
+def get_library():
+    return render_template('library.html', book=mongo.db.book.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
