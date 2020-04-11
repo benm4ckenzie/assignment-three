2d867f56a5a1c82fc92a416e74645504265ca05b
benm4ckenzie <b_m@me.com>
2020-04-11 16:26:02 +0000

    added library images and home page with slider

--- app.py
+++ app.py
@@ -8,12 +8,20 @@ app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'book_review'
 app.config["MONGO_URI"] = 'mongodb+srv://benm4ckenzie:Istanbul5x@myfirstcluster-beoum.mongodb.net/book_review?retryWrites=true&w=majority'
 
+
 mongo = PyMongo(app)
+
 
 @app.route('/')
 @app.route('/get_library')
 def get_library():
     return render_template('library.html', book=mongo.db.book.find())
+
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
