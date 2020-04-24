616395fab69aafb7ffc71a4962390ec17211f5bc
benm4ckenzie <b_m@me.com>
2020-04-24 15:29:48 +0000

    added collection to drop down menu on add book page

--- app.py
+++ app.py
@@ -18,15 +18,28 @@ def get_library():
     return render_template('library.html', book=mongo.db.book.find())
 
 
+@app.route('/')
+@app.route('/my_library')
+def my_library():
+    return render_template('mylibrary.html', book=mongo.db.book.find()) 
+
+
 @app.route('/home')
 def home():
     return render_template('home.html')
+
+@app.route('/mylibrary')
+def mylibrary():
+    return render_template('mylibrary.html')
 
 
 @app.route('/add_book')
 def add_book():
     return render_template('addbook.html',
-    book=mongo.db.book.find())
+    title = mongo.db.title.find(),
+    author = mongo.db.author.find(),
+    genre = mongo.db.genre.find(),
+    publisher = mongo.db.publisher.find())
 
 
 @app.route('/submit_book', methods=['POST'])
@@ -34,6 +47,13 @@ def submit_book():
     book = mongo.db.book
     book.insert_one(request.form.to_dict())
     return redirect(url_for('get_library'))
+
+
+@app.route('/add_to_mylibrary', methods=['POST'])
+def add_to_mylibrary():
+    reader = mongo.db.reader
+    reader.insert_one(request.form.to_dict())
+    return redirect(url_for('mylibrary'))
 
 
 @app.route('/edit_book/<book_id>')
